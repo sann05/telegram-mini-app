c69944474b78b07a46c45834adf479a3afc871c1
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2024-09-03 15:23:02 +0200

    Updated app

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,12 @@ import { UserOutlined, TrophyOutlined, LogoutOutlined } from '@ant-design/icons'
 import { Button } from 'antd';
 import buttonSvg from './assets/button.png';
 import moneySvg from './assets/money.png';
+import { TonConnectUIProvider, TonConnectButton } from '@tonconnect/ui-react';
+
 
 function App() {
   const [coinCount, setCoinCount] = useState<number>(0);
-  const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
+  const selectedCountry = 'Poland';
   const [userId, setUserId] = useState<string>('');
   const [currentView, setCurrentView] = useState<string>('coin');
 
@@ -23,7 +25,6 @@ function App() {
         const user = await getUser(storedUserId);
         if (user) {
           setCoinCount(user.coins);
-          setSelectedCountry(user.country);
         }
       } else {
         const newUserId = 'You';
@@ -43,45 +44,15 @@ function App() {
     }
   };
 
-  const handleCountryChange = async (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const users = await getAllUsers();
-    if (users.length === 0) {
-      await populateDB();
-    }
-    const country = event.target.value;
-    setSelectedCountry(country);
-    if (userId) {
-      await addUser({ userid: userId, country, coins: 0 });
-    }
-  };
-
   const handleLogout = async () => {
     localStorage.removeItem('userId');
     await clearDB(); // Clear the IndexedDB database
-    setSelectedCountry(null);
     setCoinCount(0);
     setUserId('');
     setCurrentView('coin');
   };
 
   const renderContent = () => {
-    if (!selectedCountry) {
-      return (
-        <div className={styles.countrySelection}>
-          <h1>Select Your Country</h1>
-          <select onChange={handleCountryChange} defaultValue="">
-            <option value="" disabled>
-              Choose your country
-            </option>
-            {countries.map((country) => (
-              <option key={country} value={country}>
-                {country}
-              </option>
-            ))}
-          </select>
-        </div>
-      );
-    }
 
     if (currentView === 'coin') {
       return (
@@ -90,7 +61,6 @@ function App() {
             <img src={moneySvg} alt="money" className={styles.scoreImg} />
             <h1>{coinCount}</h1>
           </div>
-          <p>Country: {selectedCountry}</p>
           <img src={buttonSvg} alt="Click to earn coins" className={styles.clickButton} onClick={handleButtonClick} />
         </div>
       );
@@ -104,16 +74,22 @@ function App() {
   };
 
   return (
+    <TonConnectUIProvider 
+      manifestUrl="https://telegram-mini-app-phi-nine.vercel.app/tonconnect-manifest.json"
+      actionsConfiguration={{
+        twaReturnUrl: 'https://t.me/zombietrainbot',
+    }}
+    >
     <div className={styles.app}>
+      <TonConnectButton className={styles.tonButton}/>
       {renderContent()}
-      {selectedCountry && (
-        <div className={styles.menu}>
+      <div className={styles.menu}>
           <Button ghost className={styles.btn} onClick={() => setCurrentView('coin')} shape="circle" icon={<UserOutlined className={styles.icon} />} />
           <Button ghost className={`${styles.btn} ${styles.invisible}`} onClick={handleLogout} shape="circle" icon={<LogoutOutlined className={styles.icon} />} />
           <Button ghost className={styles.btn} onClick={() => setCurrentView('leaderboard')} shape="circle" icon={<TrophyOutlined className={styles.icon} />} />
-        </div>
-      )}
+      </div>
     </div>
+    </TonConnectUIProvider>
   );
 }
 
